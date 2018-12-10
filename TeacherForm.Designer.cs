ead46cf8dc021237b47e5c152f001dedbea08324
struggle-team <45408187+struggle-team@users.noreply.github.com>
2018-12-09 23:28:01 -0600

    Add files via upload

---
+++ TeacherForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DesktopApp1
+{
+    partial class TeacherForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.frmLabel = new System.Windows.Forms.Label();
+            this.quesLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // frmLabel
+            // 
+            this.frmLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.frmLabel.Location = new System.Drawing.Point(53, 30);
+            this.frmLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.frmLabel.Name = "frmLabel";
+            this.frmLabel.Size = new System.Drawing.Size(408, 53);
+            this.frmLabel.TabIndex = 0;
+            this.frmLabel.Text = "Teacher Group Formation Generator";
+            this.frmLabel.Click += new System.EventHandler(this.frmLabel_Click);
+            // 
+            // quesLabel
+            // 
+            this.quesLabel.AutoSize = true;
+            this.quesLabel.Location = new System.Drawing.Point(60, 112);
+            this.quesLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.quesLabel.Name = "quesLabel";
+            this.quesLabel.Size = new System.Drawing.Size(359, 17);
+            this.quesLabel.TabIndex = 2;
+            this.quesLabel.Text = "Please specify the total number of students in the class:";
+            this.quesLabel.Click += new System.EventHandler(this.quesLabel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(426, 112);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 148);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Please specify the total number of groups you would like:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(426, 155);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(99, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // TeacherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.quesLabel);
+            this.Controls.Add(this.frmLabel);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "TeacherForm";
+            this.Text = "TeacherForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label frmLabel;
+        private System.Windows.Forms.Label quesLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ TeacherForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApp1
+{
+    public partial class TeacherForm : Form
+    {
+        public TeacherForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quesLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
